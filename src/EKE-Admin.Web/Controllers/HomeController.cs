64e45af6a1a6e60aeb15e8bae2ab379f4bb12284
Deak Levente <Dlevi93@outlook.com>
2017-10-18 17:09:27 +0300

    admin counter

--- src/EKE-Admin.Web/Controllers/HomeController.cs
+++ src/EKE-Admin.Web/Controllers/HomeController.cs
@@ -4,15 +4,55 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+using EKE.Service.Services.Admin;
+using EKE.Service.Services;
+using EKE.Service.Services.Admin.Muzeum;
+using EKE_Admin.Web.ViewModels;
 
 namespace EKE_Admin.Web.Controllers
 {
+    [Authorize]
+    [AutoValidateAntiforgeryToken]
     public class HomeController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly IMagazineService _magService;
+        private readonly IArticleService _articleService;
+        private readonly IMuseumService _museumService;
+
+        public HomeController(
+            IMagazineService magazineService,
+            IArticleService articleService,
+            IMuseumService museumService,
+            IMapper mapperService)
+        {
+            _magService = magazineService;
+            _articleService = articleService;
+            _museumService = museumService;
+            _mapper = mapperService;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var homeVM = new HomeVM();
+            var gyoparResult = _magService.GetAllMagazinesIncluding();
+            if (gyoparResult.IsOk())
+            {
+                homeVM.GY_Magazines = gyoparResult.Data.Count;
+                homeVM.GY_Articles = gyoparResult.Data.SelectMany(x => x.Articles).Count();
+                homeVM.GY_MediaElements = gyoparResult.Data.SelectMany(x => x.MediaElements.Where(y => y.Scope == EKE.Data.Entities.Enums.MediaTypesScope.Cover)).Count();
+            };
+
+            var museumResult = _museumService.GetAllElements();
+            if (museumResult.IsOk())
+            {
+                homeVM.M_Elements = museumResult.Data.Count;
+                homeVM.M_MediaElements = museumResult.Data.SelectMany(x => x.MediaElement.Where(y => y.Scope == EKE.Data.Entities.Enums.MediaTypesScope.Museum)).Count();
+                homeVM.M_Tags = museumResult.Data.SelectMany(x => x.Tags).Count();
+            };
+            return View(homeVM);
         }
 
         [Authorize(Roles = "admin")]
---
+++ src/EKE-Admin.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EKE_Admin.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public int GY_Articles { get; set; }
+        public int GY_Magazines { get; set; }
+        public int GY_Orders { get; set; }
+        public int GY_MediaElements { get; set; }
+
+        public int M_Elements { get; set; }
+        public int M_Category { get; set; }
+        public int M_Tags { get; set; }
+        public int M_MediaElements { get; set; }
+        public HomeVM()
+        {
+            GY_Articles = 0;
+            GY_Magazines = 0;
+            GY_Orders = 0;
+            GY_MediaElements = 0;
+            M_Elements = 0;
+            M_Category = 0;
+            M_Tags = 0;
+            M_MediaElements = 0;
+        }
+    }
+}
